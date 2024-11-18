35d68a4c109232f119129fce51800b3c18d72616
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-11-18 19:50:48 -0300

    C12 - Back-end - Implementando a Listagem

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -3,8 +3,9 @@ package br.com.ifpe.oxefood.modelo.produto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ifpe.oxefood.modelo.produto.Produto;
 import jakarta.transaction.Transactional;
-
+import java.util.List;
 @Service
 public class ProdutoService {
    
@@ -17,6 +18,16 @@ public class ProdutoService {
        produto.setHabilitado(Boolean.TRUE);
        return repository.save(produto);
    }
+    public List<Produto> listarTodos() {
+  
+        return repository.findAll();
+    }
+
+    public Produto obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
+
 
 }
 
